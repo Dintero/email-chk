3974251904746cb086de131c944c9b589f471600
Frode Aannevik <frode@dintero.com>
2023-02-14 08:53:24 +0100

    ci(build|release): Configure PR and release build

--- dist/utils.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const emailRegex: RegExp;
-export declare const isEmail: (email: string) => boolean;
-export declare const validateEmail: (email: string | undefined) => boolean;
-export declare const levenstein: (a: string, b: string) => number;
-export declare const getDomain: (website?: string) => string | undefined;
